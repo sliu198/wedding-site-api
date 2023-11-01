da58b0fb6a6b23c92e43700ce23ece25cf8e9a1c
Solomon Liu <sliu198@gmail.com>
2023-11-01 09:43:18 -0700

    grant api permission to lambda

--- __tests__/unit/handlers/api-lambda.js
+++ __tests__/unit/handlers/api-lambda.js
@@ -1,7 +1,59 @@
 // Import all functions from hello-from-lambda.js
 const lambda = require('../../../src/handlers/api-lambda.js');
 
-describe('Test for hello-from-lambda', () => {
-    // This test invokes the hello-from-lambda Lambda function and compares the result
-    it.todo('Verifies successful response');
+describe('api-lambda', function() {
+  describe('index', function() {
+    it('returns 404 for bad path', async function() {
+      const response = await lambda.indexHandler({
+        httpMethod: 'POST',
+        path: '',
+        queryStringParameters: {},
+        headers: {},
+        body: ''
+      });
+
+      confirmJsonResponse(
+        response,
+        { message: 'Not Found'},
+        {expectedStatusCode: 404}
+        )
+    });
+  });
+
+  describe('/subscribe', function () {
+    it('confirm successful request', async function() {
+      const response = await lambda.indexHandler({
+        httpMethod: 'POST',
+        path: '/subscribe',
+        queryStringParameters: {},
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({name: 'abc', email: 'def@ghi'})
+      });
+
+      confirmJsonResponse(
+        response,
+        expect.objectContaining({
+          message: expect.stringMatching(/Hello.*abc.*def@ghi/)
+        }),
+      )
+    })
+  });
 });
+
+function confirmJsonResponse(response, expectedBody, {expectedStatusCode = 200} = {}) {
+  const {
+    body,
+  } = response
+
+  expect(response).toEqual(expect.objectContaining({
+    statusCode: expectedStatusCode,
+    headers: expect.objectContaining({
+      'content-type': 'application/json',
+    })
+  }));
+
+  const parsedBody = JSON.parse(body);
+  expect(parsedBody).toEqual(expectedBody)
+}
